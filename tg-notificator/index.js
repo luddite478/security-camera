609b2c3995430e90fbec045f4e873dee5a875a8e
luddite478 <luddite478@gmail.com>
2019-05-05 04:57:10 +0300

    add photo monitoring

--- tg-notificator/index.js
+++ tg-notificator/index.js
@@ -1,20 +1,37 @@
 require('dotenv').config()
 const fs = require('fs')
 const TelegramBot = require('node-telegram-bot-api')
+const chokidar = require('chokidar')
+const findRemoveSync = require('find-remove')
+
+const watchFolder = "/tmp/photos"
 
 const token = process.env.TG_TOKEN
 const chat_id = process.env.TG_CHAT_ID
 
 const bot = new TelegramBot(token, {polling: true})
 
-const photo = '/tmp/photos/photo1.png'
-
-bot.sendPhoto(chat_id, photo).then(res => console.log(res))
-
-try {
-  if (fs.existsSync(photo)) {
-    console.log("FILE EXISTS")
+const watcher = chokidar.watch(watchFolder, {
+  ignored: /^\./,
+  persistent: true,
+  awaitWriteFinish: {
+    stabilityThreshold: 2000,
+    pollInterval: 100
   }
-} catch(err) {
-  console.error(err)
+})
+
+watcher
+  .on('add', sendToTgChannel)
+
+setInterval(findRemoveSync.bind(this, watchFolder, {age: {seconds: 30}}), 120)
+
+function sendToTgChannel(photoPath){
+  try {
+    if (fs.existsSync(photoPath)) {
+       console.log("Found new photo: ", photoPath)
+       bot.sendPhoto(chat_id, photoPath).then(res => console.log(res))
+    }
+  } catch(err) {
+    console.error(err)
+  }
 }
