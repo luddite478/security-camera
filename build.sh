dc5dfc395ed8cc7fd967e235b4e7a74e590d6d22
luddite478 <luddite478@gmail.com>
2019-05-09 18:40:37 +0300

    webcamera initial

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-cd rtmp-authenticator && docker build -t rtmp-authenticator .
-cd ../nginx-rtmp && docker build -t nginx-rtmp .
-cd ../tg-notificator && docker build -t tg-notificator .
+cd tg-notificator && docker build -t tg-notificator .
 cd ../video-cutter && docker build -t video-cutter .
-
